502b9c7768c4ccdff159ef56c3a79f2bd4a17684
M Muaz Shahzad <muazshahzad667@gmail.com>
2023-02-17 22:13:07 +0500

    Add files via upload

---
+++ First_Program/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+   <calculator></calculator>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'calculator-display',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+
+</style>
